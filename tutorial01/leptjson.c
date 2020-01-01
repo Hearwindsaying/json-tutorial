e9f682d0c8353397edc882709b8b1eb6c16e3f5e
hearwindsaying <hearwindsaying@gmail.com>
2020-01-02 07:36:34 +0800

    finish tutorial01

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -5,6 +5,7 @@
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
+    /* Current parsing position of json string. */
     const char* json;
 }lept_context;
 
@@ -24,9 +25,30 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_true(c, v);
+        case 'f':  return lept_parse_false(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
@@ -38,7 +60,16 @@ int lept_parse(lept_value* v, const char* json) {
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+
+    int parseErrorCode = lept_parse_value(&c, v);
+    if (parseErrorCode != LEPT_PARSE_OK)
+        return parseErrorCode;
+
+    lept_parse_whitespace(&c);
+    if (*c.json != '\0')
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    else
+        return parseErrorCode;
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -9,13 +9,24 @@ typedef struct {
 
 enum {
     LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_EXPECT_VALUE,        /* Json string is empty. */
+    LEPT_PARSE_INVALID_VALUE,       /* Not supported value. */
+    LEPT_PARSE_ROOT_NOT_SINGULAR    /* After a json value, a blank is followed by a character. */
 };
 
+/**
+ * @brief Parse |json| string and write to |v|.
+ * @param[in]  json the json string to be parsed
+ * @param[out] v    value
+ * @return Return error code for parsing.
+ */
 int lept_parse(lept_value* v, const char* json);
 
+/**
+ * @brief Retrieve a type from |v|.
+ * @param[in] v value that has been parsed before
+ * @return Return type stored in v.
+ */
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
